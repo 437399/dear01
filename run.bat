b0742ea98a38a9785a0bb2f1a1ab9d2dbf7127a2
437399 <jeetendra.login@gmail.com>
2017-07-05 21:14:57 +0530

    batch push
    aaaf

--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
 set projectLocation=https://github.com/437399/dear01
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
+java -cp %projectLocation%\lib\*;%projectLocation%\bin org.testng.TestNG testng.xml
+

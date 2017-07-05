f7434927aa1bc8e793ca3dd0bcc1878e8ecab869
437399 <jeetendra.login@gmail.com>
2017-07-05 07:19:30 +0530

    1st push
    push

---
+++ src/hellodear01/lohit01.java
@@ -0,0 +1,13 @@
+package hellodear01;
+
+import org.testng.annotations.Test;
+
+public class lohit01 {
+	
+	@Test()
+	public void launch(){
+		System.out.println("testing testing");
+		System.out.println("testing testing");
+	}
+
+}

1018818f4a203dbed8d6c639e52e5869675c3d29
437399 <jeetendra.login@gmail.com>
2017-07-06 06:35:07 +0530

    ffasfsfasfasfasf

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
-set classpath=D:\GitVersion\learning001\dear01\lib*;D:\GitVersion\learning001\dear01\bin
+set cp=D:\GitVersion\learning001\dear01\lib*;D:\GitVersion\learning001\dear01\bin
 
-java org.testng.TestNG testng.xml
+java -cp org.testng.TestNG testng.xml
 
 

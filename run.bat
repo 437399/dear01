7c4c4d85580022ee3abe26aa600f2b26f41d716d
437399 <jeetendra.login@gmail.com>
2017-07-06 06:19:51 +0530

    zfsdfs
    asfasfafasf

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 set projectLocation=https://github.com/437399/dear01
-java -cp %projectLocation%\lib\*;%projectLocation%\bin org.testng.TestNG testng.xml
+java -cp %projectLocation%/lib/*;%projectLocation%/bin org.testng.TestNG testng.xml
 

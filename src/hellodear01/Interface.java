eaf3e2e7a5be18504d28661a1572660bb2b5d32f
437399 <jeetendra.login@gmail.com>
2017-08-21 08:04:09 +0530

    Commit on 21-08-2017

---
+++ src/hellodear01/Interface.java
@@ -0,0 +1,8 @@
+package hellodear01;
+
+interface Interface {
+	int a=3;
+	public void func();
+	
+
+}
---
+++ src/hellodear01/thissuper.java
@@ -0,0 +1,37 @@
+package hellodear01;
+abstract class A{
+	public void f1(){
+	System.out.println("Hello my friend class A");	
+	}
+	abstract void function();
+}
+
+ class B extends A{
+	public void f1(){
+		System.out.println("Hello my friend class B");
+	}
+		
+		void function(){
+			
+		}
+	
+	}
+
+	
+
+public class thissuper implements hellodear01.Interface {
+	public static void main(String args[]){
+		B obj1=new B();
+
+  public void func(){
+	  
+	   
+   }
+	
+			
+	
+
+
+
+}
+
